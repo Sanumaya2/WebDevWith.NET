71c9416d35520ee47dd807cddf5da17b4a8e7e16
Sanumaya2 <goritara23456@gmail.com>
2024-05-03 10:28:49 -0700

    use FK in courseId

--- CollegeMonitor/Data/Models/Session.cs
+++ CollegeMonitor/Data/Models/Session.cs
@@ -8,8 +8,13 @@ public class Session
       public string? Details {get ; set;}
        public DateTime Start {get ; set;}
        public float DurationInHours {get ; set;}
+       public int? CourseId {get ; set ;} //fk always made in mini side or secondary table
+       public Course? Course {get ; set ;} //navigation property
+       
 }
 
+//Course has multiple session=one to many 
+// 
 // class A 
 // {
 
