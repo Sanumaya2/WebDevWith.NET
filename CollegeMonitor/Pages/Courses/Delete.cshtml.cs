4e5402dfe01e98314bb8b5968b48962d453b37b0
Sanumaya2 <goritara23456@gmail.com>
2024-05-21 14:11:47 -0700

    Add link

--- CollegeMonitor/Pages/Courses/Delete.cshtml.cs
+++ CollegeMonitor/Pages/Courses/Delete.cshtml.cs
@@ -1,19 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using NetStarter.Basics.DataAccess;
+
 public class DeleteCourseModel : PageModel
 {
+    private object?[]? id;
+
     [BindProperty]
     public Course Course { get; set; }
-    public void OnGet(int id)
+    public List<Course> Courses { get; set; }
+    public async Task OnGetAsync(int id)
     {
-        CollegeDbContext db = new();
-        Course = db.Courses.Find(id);
+        // CollegeDbContext db = new();
+        // Course = db.Courses.Find(id);
+        HttpClient http = new HttpClient();
+        Courses = await http.GetFromJsonAsync<List<Course>>("http://localhost:5017/courses");
     }
 
     public IActionResult OnPost()
     {
         CollegeDbContext db = new();
+        var course = db.Courses.Find(id);
         db.Courses.Remove(Course);
         db.SaveChanges();
 
--- CollegeMonitor/Pages/Courses/Edit.cshtml.cs
+++ CollegeMonitor/Pages/Courses/Edit.cshtml.cs
@@ -1,14 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using NetStarter.Basics.DataAccess;
+
 public class EditCourseModel : PageModel
 {
     [BindProperty]
     public Course Course { get; set; }
-    public void OnGet(int id)
+       public List<Course> Courses { get; set; }
+    
+    public async Task OnGetAsync(int id)
     {
-        CollegeDbContext db = new();
-        Course = db.Courses.Find(id);
+        // CollegeDbContext db = new();
+        // Course = db.Courses.Find(id);
+         HttpClient http=new HttpClient();
+        //Courses = await http.GetFromJsonAsync<List<Course>>("http://localhost:5017/courses/{id}");
+        Courses = await http.GetFromJsonAsync<List<Course>>("http://localhost:5017/courses");
+        
     }
 
     public IActionResult OnPost()
@@ -16,6 +22,7 @@ public class EditCourseModel : PageModel
         CollegeDbContext db = new();
         db.Courses.Update(Course);
         db.SaveChanges();
+      
 
         return RedirectToPage("Courses");
     }
